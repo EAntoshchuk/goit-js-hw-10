1c8ac642e659889dd0f32a8c8bfc26cd60aa7229
Eduard Antoshchuk <e.antoshchuk@gmail.com>
2023-04-02 19:34:44 +0300

    hw10_done

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import debounce from 'lodash.debounce';
 import './css/styles.css';
 import country_card from '../src/templates/country-card.hbs';
 import country_list from '../src/templates/country-list.hbs';
+import { fetchCountry } from './js/fetch-countries';
 import getRefs from './js/get-refs';
 
 const BASE_URL = 'https://restcountries.com/v3.1/name';
-const options = {
-  fields: 'name,capital,population,flags,languages',
-};
+// const options = {
+//   fields: 'name,capital,population,flags,languages',
+// };
 
 const DEBOUNCE_DELAY = 300;
 const refs = getRefs();
@@ -31,36 +32,37 @@ function searchCountry(event) {
           'Too many matches found. Please enter a more specific name.'
         );
         return;
-      } else if (seachedCountry.length >= 2 && data.length <= 10) {
+      } else if (data.length >= 2 && data.length <= 10) {
+        refs.countryInfo.innerHTML = '';
         return renderCountryList(data);
       }
-      return renderCountryInfo(data);
+      refs.countryList.innerHTML = '';
+      renderCountryInfo(data);
     })
     .catch(error => {
+      refs.countryList.innerHTML = '';
       console.log(error);
       Notiflix.Notify.failure('Oops, there is no country with that name');
     });
 }
 
-function renderCountryInfo(country) {
-  const countryInfoMarkup = country.map(
-    ({ name, capital, population, flags, languages }) => country_card
-  );
+function renderCountryInfo(countries) {
+  const countryInfoMarkup = country_card(countries);
   refs.countryInfo.innerHTML = countryInfoMarkup;
 }
 
-function renderCountryList(country) {
-  const countryListMarkup = country.map(({ name, flags }) => country_list);
+function renderCountryList(countries) {
+  const countryListMarkup = country_list(countries);
   refs.countryList.innerHTML = countryListMarkup;
 }
 
-function fetchCountry(countryName) {
-  return fetch(
-    `${BASE_URL}/${countryName}?fields=name,capital,population,flags,languages`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-}
+// function fetchCountry(countryName) {
+//   return fetch(
+//     `${BASE_URL}/${countryName}?fields=name,capital,population,flags,languages`
+//   ).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
